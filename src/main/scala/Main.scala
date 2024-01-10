b7f52f1cdd2e858920b2a634dcb60e22120a37b1
LancelotDucoeur <lancelot.ducoeur@gmail.com>
2024-01-10 15:08:29 +0100

    initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
+
+object Main {
+	def main(args: Array[String]) {
+		System.out.println("coucou");
+	}
+}
